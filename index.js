00bf4036ca315fd7f804e83891f24c0dfd4026dd
Steven Mentzer <stevenfmentzer@gmail.com>
2023-11-06 15:49:41 -0500

    Labwork done

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code here!
+const mainElement = document.querySelector('main#main');
+mainElement.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.textContent = 'STEVEN is the champion';
